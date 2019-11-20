e57469b3822a9f6683fe1285421b91d5c6dfe10f
Vasily Ulianov <vasily@me.com>
2019-11-20 19:48:31 +0300

    Add pay button

--- Sources/UI/View Controller/Input/InputViewController.swift
+++ Sources/UI/View Controller/Input/InputViewController.swift
@@ -25,17 +25,24 @@ class InputViewController: UIViewController {
         super.viewDidLoad()
         
         title = network.label
-        view.backgroundColor = UIColor.white
         
         let tableView = addTableView()
         tableView.register(InputTableViewCell.self)
         tableView.dataSource = tableController
         tableView.delegate = tableController
+        if #available(iOS 13.0, *) {
+            tableView.backgroundColor = UIColor.systemBackground
+        } else {
+            tableView.backgroundColor = UIColor.white
+        }
         self.tableView = tableView
+        
+        let tableFooterView = makeTableViewFooter()
+        tableView.tableFooterView = tableFooterView
     }
     
     private func addTableView() -> UITableView {
-        let tableView = UITableView(frame: CGRect.zero, style: .grouped)
+        let tableView = UITableView(frame: .zero, style: .grouped)
 
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PaymentListTableViewCell.self)
@@ -51,6 +58,39 @@ class InputViewController: UIViewController {
         ])
 
         return tableView
+    }
+    
+    private func makeTableViewFooter() -> UIView {
+        let contentView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 60))
+        
+        let button = UIButton(frame: .zero)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = view.tintColor
+        let title = NSAttributedString(string: "Pay", attributes: [
+            .font: UIFont.systemFont(ofSize: UIFont.buttonFontSize, weight: .semibold),
+            .foregroundColor: UIColor.white
+        ])
+        
+        button.setAttributedTitle(title, for: .normal)
+        button.addTarget(self, action: #selector(dismissNavigation), for: .touchUpInside)
+                
+        contentView.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            button.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            button.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
+        
+        let trailing = button.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
+        trailing.priority = .defaultHigh
+        trailing.isActive = true
+        
+        return contentView
+    }
+    
+    @objc private func dismissNavigation() {
+        navigationController?.dismiss(animated: true, completion: nil)
     }
 }
 #endif
