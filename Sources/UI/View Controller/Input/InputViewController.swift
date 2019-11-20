9d05081ed25d696abe7ea5446fa63d5b8860277e
Vasily Ulianov <vasily@me.com>
2019-11-21 00:39:38 +0300

    Move pay button to table footer

--- Sources/UI/View Controller/Input/InputViewController.swift
+++ Sources/UI/View Controller/Input/InputViewController.swift
@@ -46,7 +46,7 @@ class InputViewController: UIViewController {
 
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PaymentListTableViewCell.self)
-        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = 50
         
         view.addSubview(tableView)
 
@@ -62,70 +62,34 @@ class InputViewController: UIViewController {
     
     private func makeTableViewHeader() -> UIView {
         let contentView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 200))
-        
-        // Image Content View
-        let imageContentView = UIView(frame: .zero)
-        contentView.addSubview(imageContentView)
-        if #available(iOS 13.0, *) {
-//            imageContentView.backgroundColor = UIColor.systemGray6
-        } else {
-            imageContentView.backgroundColor = UIColor.systemGray
-        }
-        
-        imageContentView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            imageContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageContentView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageContentView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 2/3)
-        ])
-        
-        let imageContentViewTrailing = imageContentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-        imageContentViewTrailing.priority = .defaultHigh
-        imageContentViewTrailing.isActive = true
+        contentView.preservesSuperviewLayoutMargins = true
         
         // Image
-        let imageView: UIImageView
+        let imageView: UIView
         if let image = network.logo?.image {
             imageView = UIImageView(image: image)
         } else {
             imageView = UIImageView(frame: .zero)
         }
-        imageContentView.addSubview(imageView)
-        
+        contentView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+                
         imageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: imageContentView.layoutMarginsGuide.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: imageContentView.layoutMarginsGuide.centerYAnchor)
+            imageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 8*4),
+            imageView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor, constant: 8*4),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8*4),
         ])
         
-        // Title
-        let title = UILabel(frame: .zero)
-        contentView.addSubview(title)
-        
-        title.text = network.label
-        title.textAlignment = .center
-        if #available(iOS 11.0, *) {
-            title.font = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize)
-        } else {
-            // Fallback on earlier versions
-        }
-        
-        title.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            title.topAnchor.constraint(equalTo: imageContentView.bottomAnchor, constant: 16),
-            title.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            title.bottomAnchor.constraint(greaterThanOrEqualTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -24)
-        ])
-        
-        let titleTrailing = title.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
-        titleTrailing.priority = .defaultHigh
-        titleTrailing.isActive = true
+        let imageContentViewTrailing = imageView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor, constant: -8*4)
+        imageContentViewTrailing.priority = .defaultHigh
+        imageContentViewTrailing.isActive = true
         
         return contentView
     }
     
     private func makeTableViewFooter() -> UIView {
-        let contentView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 60))
+        let contentView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 76))
         
         let button = UIButton(frame: .zero)
         button.layer.cornerRadius = 10
@@ -137,13 +101,13 @@ class InputViewController: UIViewController {
         
         button.setAttributedTitle(title, for: .normal)
         button.addTarget(self, action: #selector(dismissNavigation), for: .touchUpInside)
-                
+
         contentView.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             button.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
             button.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
-            button.topAnchor.constraint(equalTo: contentView.topAnchor)
+            button.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16)
         ])
         
         let trailing = button.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
