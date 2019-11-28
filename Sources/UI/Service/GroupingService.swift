ee6eb5f02bfcb3b763f3978407f36568f9a69b6a
Vasily Ulianov <vasily@me.com>
2019-11-28 13:16:59 +0300

    Introduce RawProvider
    We can't use non-swift files (like `.strings`) in Swift Package Manager, that's why we store it as static variables in that class's extensions to make it look similar to Android SDK.

--- Sources/UI/Service/GroupingService.swift
+++ Sources/UI/Service/GroupingService.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class GroupingService {
     private func get() throws -> [Rule] {
-        guard let data = groupsJson.data(using: .utf8) else {
+        guard let data = RawProvider.groupsJSON.data(using: .utf8) else {
             throw InternalError(description: "Unable to convert data to utf8")
         }
         
@@ -76,43 +76,4 @@ private extension Sequence where Element == GroupingService.Rule {
         
         return nil
     }
-}
-
-// MARK: - Raw JSON
-
-private var groupsJson: String {
-"""
-{
-    "items": [
-        {
-            "code": "DISCOVER",
-            "regex": "^(6[045]|62212[6-9]|6221[3-9][0-9]|622[2-8][0-9]{2}|6229[01][0-9]|62292[0-5])[0-9]*$"
-        },
-        {
-            "code": "MASTERCARD",
-            "regex": "^(5[0-5]|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]*$"
-        },
-        {
-            "code": "DINERS",
-            "regex": "^(2014|2149|30[059]|3[689])[0-9]*$"
-        },
-        {
-            "code": "UNIONPAY",
-            "regex": "^62[0-9]*$"
-        },
-        {
-            "code": "AMEX",
-            "regex": "^3[47][0-9]*$"
-        },
-        {
-            "code": "JCB",
-            "regex": "^35[0-9]*$"
-        },
-        {
-            "code": "VISA",
-            "regex": "^4[0-9]*$"
-        }
-    ]
-}
-"""
 }
