06c98053ee95f99c70eec324e5a4b4baa5f5213c
Vasily Ulianov <vasily@me.com>
2020-01-02 01:32:36 +0300

    Color navigation bar

--- Sources/UI/Theme/UIColor+.swift
+++ Sources/UI/Theme/UIColor+.swift
@@ -5,5 +5,13 @@ extension UIColor {
     static var text: UIColor {
         return UIColor(white: 66.0 / 255.0, alpha: 1.0)
     }
+    
+    static var detailedText: UIColor {
+        return UIColor(white: 143.0 / 255.0, alpha: 1.0)
+    }
+    
+    static var navigationBarTintColor: UIColor {
+        return UIColor(red: 0.0, green: 137.0 / 255.0, blue: 64.0 / 255.0, alpha: 1.0)
+    }
 }
 #endif
--- Sources/UI/View Controller/PaymentNavigationController.swift
+++ Sources/UI/View Controller/PaymentNavigationController.swift
@@ -3,7 +3,12 @@
 import UIKit
 
 @objc public class PaymentNavigationController: UINavigationController {
-
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = .navigationBarTintColor
+        navigationBar.tintColor = .white
+        navigationBar.barStyle = .black
+    }
 }
 
 #endif
