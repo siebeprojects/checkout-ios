f8dc30a66d2f5631d0633b6aef04a91cc8d079f4
Vasily Ulianov <esorix@gmail.com>
2020-04-22 03:54:44 +0300

    Naming changes

--- Sources/UI/Scenes/Input/TextFormatter/CardFormatProcessor.swift
+++ Sources/UI/Scenes/Input/TextFormatter/CardFormatProcessor.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 struct CardFormatProcessor: TextFormatProcessor {
-    private let cardNumberSeparator = " "
+    private let separator = " "
     private let groupSize = 4
 
     func format(string: String) -> String {
-        let setWithNumberSeparator = CharacterSet.init(charactersIn: cardNumberSeparator)
+        let setWithNumberSeparator = CharacterSet.init(charactersIn: separator)
 
         var formattedText = string.remove(charactersIn: setWithNumberSeparator)
-        formattedText.insert(separator: cardNumberSeparator, every: groupSize)
+        formattedText.insert(separator: separator, every: groupSize)
         formattedText = formattedText.trimmingCharacters(in: setWithNumberSeparator)
 
         return formattedText
