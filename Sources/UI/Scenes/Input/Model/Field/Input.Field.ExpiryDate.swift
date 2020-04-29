44b4869f9453e65f451ba2abb84d8154a36394c0
Vasily Ulianov <esorix@gmail.com>
2020-04-29 15:26:18 +0300

    Add validation to expiration date

--- Sources/UI/Scenes/Input/Model/Field/Input.Field.ExpiryDate.swift
+++ Sources/UI/Scenes/Input/Model/Field/Input.Field.ExpiryDate.swift
@@ -20,24 +20,28 @@ extension Input.Field {
     }
 }
 
-//extension Input.Field.ExpiryDate: Validatable {
-//    func localize(error: Input.Field.Validation.ValidationError) -> String {
-//        switch error {
-//        case .invalidValue, .incorrectLength: return translator.translation(forKey: "error.INVALID_EXPIRY_MONTH")
-//        case .missingValue: return translator.translation(forKey: "error.MISSING_EXPIRY_MONTH")
-//        }
-//    }
-//
-//    var isPassedCustomValidation: Bool {
-//        guard let expiryYear = expiryYearField?.value else {
-//            // Don't check if year is not filled, that have to be done when is filled option is used.
-//            return true
-//        }
-//
-//        let validationResult = Input.Field.Validation.ExpiryDate.isInFuture(expiryMonth: value, expiryYear: expiryYear) ?? false
-//        return validationResult
-//    }
-//}
+extension Input.Field.ExpiryDate: Validatable {
+    var validationRule: Input.Field.Validation.Rule? { nil }
+    
+    func localize(error: Input.Field.Validation.ValidationError) -> String {
+        switch error {
+        case .invalidValue, .incorrectLength: return translator.translation(forKey: "error.INVALID_EXPIRY_DATE")
+        case .missingValue: return translator.translation(forKey: "error.MISSING_EXPIRY_DATE")
+        }
+    }
+
+    var isPassedCustomValidation: Bool {
+        guard value.count == 4 else {
+            return false
+        }
+        
+        let month = String(value.prefix(2))
+        let year = String("20" + value.suffix(2))
+
+        let validationResult = Input.Field.Validation.ExpiryDate.isInFuture(expiryMonth: month, expiryYear: year) ?? false
+        return validationResult
+    }
+}
 
 extension Input.Field.ExpiryDate: TextInputField {
     var maxInputLength: Int? { 4 }
