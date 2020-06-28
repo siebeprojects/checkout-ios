3c0ab77ca779f99470788eeb36db73940d8f9cd6
Vasily Ulianov <esorix@gmail.com>
2020-06-28 15:42:34 +0300

    Add interaction to PaymentResult

--- Sources/UI/Service/PaymentService/PaymentResult.swift
+++ Sources/UI/Service/PaymentService/PaymentResult.swift
@@ -2,6 +2,7 @@ import Foundation
 
 @objc public class PaymentResult: NSObject {
     public let operationResult: OperationResult
+    public var interaction: Interaction { operationResult.interaction }
     
     public init(operationResult: OperationResult) {
         self.operationResult = operationResult
