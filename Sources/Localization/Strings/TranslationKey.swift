d7581022b423d6b7a37aaa16398aaf15ca201655
Vasily Ulianov <esorix@gmail.com>
2020-09-16 14:25:54 +0300

    Add a local translation for OK button

--- Sources/Localization/Strings/TranslationKey.swift
+++ Sources/Localization/Strings/TranslationKey.swift
@@ -6,11 +6,13 @@ enum TranslationKey: String, CaseIterable {
 
     case cancelLabel = "button.cancel.label"
     case retryLabel = "button.retry.label"
+    case okLabel = "button.ok.label"
 
     var localizedString: String? {
         switch self {
         case .errorTitle: return "Oops!"
         case .errorText: return "An error occurred while handling your payment. Please try again."
+        case .okLabel: return "OK"
         default: return nil
         }
     }
