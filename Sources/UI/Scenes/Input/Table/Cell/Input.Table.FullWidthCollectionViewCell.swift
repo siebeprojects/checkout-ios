27f42abb8781b6e3d7546dc2165e30156aff2345
Vasily Ulianov <esorix@gmail.com>
2020-09-25 11:50:05 +0300

    Fix iOS14 layout issue

--- Sources/UI/Scenes/Input/Table/Cell/Input.Table.FullWidthCollectionViewCell.swift
+++ Sources/UI/Scenes/Input/Table/Cell/Input.Table.FullWidthCollectionViewCell.swift
@@ -3,10 +3,12 @@ import UIKit
 extension Input.Table {
     class FullWidthCollectionViewCell: UICollectionViewCell {
         override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
-            let layoutAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
-            layoutIfNeeded()
-            layoutAttributes.frame.size = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .fittingSizeLevel)
-            return layoutAttributes
+            let autoLayoutAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+            let targetSize = CGSize(width: layoutAttributes.frame.width, height: 0)
+            let autoLayoutSize = contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: UILayoutPriority.required, verticalFittingPriority: UILayoutPriority.defaultLow)
+            let autoLayoutFrame = CGRect(origin: autoLayoutAttributes.frame.origin, size: autoLayoutSize)
+            autoLayoutAttributes.frame = autoLayoutFrame
+            return autoLayoutAttributes
         }
     }
 }
