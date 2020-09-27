061152ff97952aa48190c276f085a9d2846abd29
Vasily Ulianov <esorix@gmail.com>
2020-09-27 23:05:40 +0300

    Change custom theme colors

--- Example/Sources/Theme+custom.swift
+++ Example/Sources/Theme+custom.swift
@@ -5,13 +5,13 @@ extension Theme {
         return Theme(
             font: UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize),
             backgroundColor: .white,
-            tableBorder: .gray,
-            tableCellSeparator: .lightGray,
-            textColor: .blue,
-            detailTextColor: .darkGray,
+            tableBorder: .init(red: 0.56, green: 0.58, blue: 0.60, alpha: 1),
+            tableCellSeparator: .init(red: 0.94, green: 0.95, blue: 0.95, alpha: 1),
+            textColor: .init(red: 0.2, green: 0.2, blue: 0.2, alpha: 1),
+            detailTextColor: .init(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.8),
             buttonTextColor: .white,
-            tintColor: .blue,
-            errorTextColor: .red
+            tintColor: .init(red: 0, green: 0.26, blue: 0.67, alpha: 1),
+            errorTextColor: .init(red: 1, green: 0.28, blue: 0, alpha: 1)
         )
     }
 }
