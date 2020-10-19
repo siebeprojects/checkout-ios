c57da34a642fce67c4ab93caf7666cf9794be58c
Vasily Ulianov <esorix@gmail.com>
2020-10-20 02:21:19 +0300

    Create VISA proceed ui test

--- Example/UITests/UITests.swift
+++ Example/UITests/UITests.swift
@@ -8,6 +8,7 @@ class UITests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
 
+        // Create payment session
         let sessionExpectation = expectation(description: "Create payment session")
         let transaction = Transaction.loadFromTemplate()
         paymentSessionService.create(using: transaction, completion: { (result) in
@@ -29,13 +30,41 @@ class UITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testVISAProceed() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        print(sessionURL.absoluteString)
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Initial screen
+        let tablesQuery = app.tables
+        if tablesQuery.buttons["Clear text"].exists {
+            tablesQuery.buttons["Clear text"].tap()
+        }
+        tablesQuery.textFields.firstMatch.typeText(sessionURL.absoluteString)
+        tablesQuery.staticTexts["Send request"].tap()
+
+        // List
+        app.tables.staticTexts["Cards"].tap()
+
+        // Input
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.textFields["Card Number"].tap()
+        collectionViewsQuery.textFields["Card Number"].typeText("4111111111111111")
+
+        collectionViewsQuery.textFields["MM / YY"].tap()
+        collectionViewsQuery.textFields["MM / YY"].typeText("1030")
+
+        collectionViewsQuery.textFields["Security Code"].tap()
+        collectionViewsQuery.textFields["Security Code"].typeText("111")
+
+        collectionViewsQuery.textFields["Name on card"].tap()
+        collectionViewsQuery.textFields["Name on card"].typeText("Test Test")
+
+        collectionViewsQuery.buttons["Pay"].tap()
+
+        // Check result
+        let interactionResult = app.alerts.firstMatch.staticTexts.element(boundBy: 1).label
+        let expectedResult = "ResultInfo: Approved Interaction code: PROCEED Interaction reason: OK Error: n/a"
+        XCTAssertEqual(expectedResult, interactionResult)
     }
 }
