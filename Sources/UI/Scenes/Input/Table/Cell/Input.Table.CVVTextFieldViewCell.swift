822c2f9d90d71fee31efb2b59a66f2bfbdcc232c
Vasily Ulianov <esorix@gmail.com>
2020-11-20 01:22:59 +0300

    [PCX-536] Show CVV hint as a popover

--- Sources/UI/Scenes/Input/Table/Cell/Input.Table.CVVTextFieldViewCell.swift
+++ Sources/UI/Scenes/Input/Table/Cell/Input.Table.CVVTextFieldViewCell.swift
@@ -12,7 +12,7 @@ protocol CVVTextFieldViewCellDelegate: class {
 }
 
 extension Input.Table {
-    class CVVTextFieldViewCell: TextFieldViewCell {
+    class CVVTextFieldViewCell: TextFieldViewCell, UIAdaptivePresentationControllerDelegate {
         weak var cvvDelegate: CVVTextFieldViewCellDelegate?
 
         override init(frame: CGRect) {
@@ -20,35 +20,39 @@ extension Input.Table {
 
             let button = UIButton()
             button.setImage(AssetProvider.iconCVVQuestionMark, for: .normal)
-            button.addTarget(self, action: #selector(hintButtonDidTap), for: .touchUpInside)
+            button.addTarget(self, action: #selector(hintButtonDidTap(button:)), for: .touchUpInside)
 
             textField.rightView = button
             textField.rightViewMode = .always
         }
 
-        @objc private func hintButtonDidTap() {
-            // Get texts
-            let title: String = model.translator.translation(forKey: "account.verificationCode.hint.where.title")
-            let text: String = model.translator.translation(forKey: "account.verificationCode.hint.where.text")
-            let okLabel: String = model.translator.translation(forKey: TranslationKey.okLabel.rawValue)
+        @objc private func hintButtonDidTap(button: UIButton) {
+            let tooltipVC = TooltipViewController()
 
-            // Get image
-            let hintImage: UIImage?
+            // Configure view controller
+            tooltipVC.detailLabelText = model.translator.translation(forKey: "account.verificationCode.hint.where.text")
+            tooltipVC.titleLabelText = model.translator.translation(forKey: "account.verificationCode.hint.where.title")
+
             if let cvvModel = model as? Input.Field.VerificationCode {
-                hintImage = cvvModel.hintImage
+                tooltipVC.image = cvvModel.hintImage
             } else {
-                hintImage = AssetProvider.cvvCard
+                tooltipVC.image = AssetProvider.cvvCard
             }
 
-            // Alert controller
-            let alertController = AlertController(title: title, message: text, preferredStyle: .alert)
-            alertController.view.tintColor = tintColor
-            alertController.setTitleImage(hintImage)
+            // Presentation
+            tooltipVC.modalPresentationStyle = .popover
 
-            let okAction = UIAlertAction(title: okLabel, style: .default, handler: nil)
-            alertController.addAction(okAction)
+            if let presentationController = tooltipVC.presentationController {
+                presentationController.delegate = self
+            }
 
-            cvvDelegate?.presentHint(viewController: alertController)
+            if let popoverController = tooltipVC.popoverPresentationController {
+                popoverController.sourceView = button
+                popoverController.sourceRect = button.bounds
+                popoverController.permittedArrowDirections = [.up, .down, .right]
+            }
+
+            cvvDelegate?.presentHint(viewController: tooltipVC)
         }
 
         required init?(coder: NSCoder) {
@@ -57,87 +61,14 @@ extension Input.Table {
     }
 }
 
-/// Adds ability to display `UIImage` above the title label of `UIAlertController`.
-/// Functionality is achieved by adding “\n” characters to `title`, to make space
-/// for `UIImageView` to be added to `UIAlertController.view`. Set `title` as
-/// normal but when retrieving value use `originalTitle` property.
-private class AlertController: UIAlertController {
-    /// - Return: value that was set on `title`
-    private(set) var originalTitle: String?
-    private var spaceAdjustedTitle: String = ""
-    private weak var imageView: UIImageView?
-    private var previousImgViewSize: CGSize = .zero
-
-    override var title: String? {
-        didSet {
-            // Keep track of original title
-            if title != spaceAdjustedTitle {
-                originalTitle = title
-            }
-        }
-    }
-
-    /// - parameter image: `UIImage` to be displayed about title label
-    func setTitleImage(_ image: UIImage?) {
-        guard let imageView = self.imageView else {
-            let imageView = UIImageView(image: image)
-            self.view.addSubview(imageView)
-            self.imageView = imageView
-            return
-        }
-        imageView.image = image
-    }
-
-    // MARK: - Layout code
-
-    override func viewDidLayoutSubviews() {
-        guard let imageView = imageView else {
-            super.viewDidLayoutSubviews()
-            return
-        }
-        // Adjust title if image size has changed
-        if previousImgViewSize != imageView.bounds.size {
-            previousImgViewSize = imageView.bounds.size
-            adjustTitle(for: imageView)
-        }
-        // Position `imageView`
-        let linesCount = newLinesCount(for: imageView)
-        let padding = Constants.padding(for: preferredStyle)
-        imageView.center.x = view.bounds.width / 2.0
-        imageView.center.y = padding + linesCount * lineHeight / 2.0
-        super.viewDidLayoutSubviews()
-    }
-
-    /// Adds appropriate number of "\n" to `title` text to make space for `imageView`
-    private func adjustTitle(for imageView: UIImageView) {
-        let linesCount = Int(newLinesCount(for: imageView))
-        let lines = (0..<linesCount).map({ _ in "\n" }).reduce("", +)
-        spaceAdjustedTitle = lines + (originalTitle ?? "")
-        title = spaceAdjustedTitle
-    }
-
-    /// - Return: Number new line chars needed to make enough space for `imageView`
-    private func newLinesCount(for imageView: UIImageView) -> CGFloat {
-        return ceil(imageView.bounds.height / lineHeight)
-    }
-
-    /// Calculated based on system font line height
-    private lazy var lineHeight: CGFloat = {
-        let style: UIFont.TextStyle = self.preferredStyle == .alert ? .headline : .callout
-        return UIFont.preferredFont(forTextStyle: style).pointSize
-    }()
-
-    struct Constants {
-        static var paddingAlert: CGFloat = 22
-        static var paddingSheet: CGFloat = 11
-        static func padding(for style: UIAlertController.Style) -> CGFloat {
-            return style == .alert ? Constants.paddingAlert : Constants.paddingSheet
-        }
-    }
-}
-
 private extension Input.Field.VerificationCode {
     var hintImage: UIImage? {
         return networkCode == "AMEX" ? AssetProvider.cvvAMEX : AssetProvider.cvvCard
+    }
+}
+
+extension Input.Table.CVVTextFieldViewCell: UIPopoverPresentationControllerDelegate {
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        .none
     }
 }
