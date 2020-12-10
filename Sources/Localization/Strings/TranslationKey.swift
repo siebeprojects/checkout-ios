728d07de4ce82cf923bb0940d28de0622cd24c06
Vasily <vasily@Vasilys-iMac.local>
2020-12-10 04:17:09 +0300

    [PCX-1160] Add new locally stored translations

--- Sources/Localization/Strings/TranslationKey.swift
+++ Sources/Localization/Strings/TranslationKey.swift
@@ -6,20 +6,27 @@
 
 import Foundation
 
+// TODO: Should be moved to a local json file
 enum TranslationKey: String, CaseIterable {
     case errorTitle = "messages.error.default.title"
     case errorText = "messages.error.default.text"
+    
+    case errorInternetTitle = "messages.error.internet.title"
+    case errorInternetText = "messages.error.internet.text"
 
     case cancelLabel = "button.cancel.label"
     case retryLabel = "button.retry.label"
     case okLabel = "button.ok.label"
 
-    var localizedString: String? {
+    var localizedString: String {
         switch self {
         case .errorTitle: return "Oops!"
         case .errorText: return "An error occurred while handling your payment. Please try again."
+        case .errorInternetTitle: return "Oops!"
+        case .errorInternetText: return "There was a problem with your internet connection"
+        case .retryLabel: return "Retry"
+        case .cancelLabel: return "Cancel"
         case .okLabel: return "OK"
-        default: return nil
         }
     }
 }
