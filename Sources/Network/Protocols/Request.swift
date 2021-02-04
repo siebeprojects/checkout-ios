17a15fcb3f6f0a4370476f85509db63c04a2bfe0
Vasily Ulianov <vasily@me.com>
2021-02-04 22:52:02 +0300

    [PCX-1272] Change URL in headers

--- Sources/Network/Protocols/Request.swift
+++ Sources/Network/Protocols/Request.swift
@@ -1,5 +1,5 @@
 // Copyright (c) 2020–2021 Payoneer Germany GmbH
-// https://www.optile.net
+// https://www.payoneer.com
 //
 // This file is open source and available under the MIT license.
 // See the LICENSE file for more information.
