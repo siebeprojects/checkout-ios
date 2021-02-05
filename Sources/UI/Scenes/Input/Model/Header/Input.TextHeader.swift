f9d49eff7039a3298342799967c22c905ff9001b
Vasily Ulianov <vasily@me.com>
2021-02-05 13:23:45 +0300

    [PCX-1272] Change copyright year

--- Sources/UI/Scenes/Input/Model/Header/Input.TextHeader.swift
+++ Sources/UI/Scenes/Input/Model/Header/Input.TextHeader.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2020–2021 Payoneer Germany GmbH
+// Copyright (c) 2021 Payoneer Germany GmbH
 // https://www.payoneer.com
 //
 // This file is open source and available under the MIT license.
