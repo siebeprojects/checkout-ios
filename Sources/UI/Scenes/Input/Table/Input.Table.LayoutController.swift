a196565997c5b9c812a45f23d611507a6e39ff59
Vasily Ulianov <vasilyul@payoneer.com>
2021-04-14 15:07:41 +0300

    [PCX-1191] Place expiry and CVV cells at the same row

--- Sources/UI/Scenes/Input/Table/Input.Table.LayoutController.swift
+++ Sources/UI/Scenes/Input/Table/Input.Table.LayoutController.swift
@@ -12,6 +12,9 @@ private extension CGFloat {
 
     /// Spacing between sections
     static var sectionSpacing: CGFloat { return 24 }
+
+    /// Spacing between sections
+    static var interitemSpacing: CGFloat { return 20 }
 }
 
 extension Input.Table {
@@ -26,13 +29,10 @@ extension Input.Table {
             self.collectionView = collectionView
             
             self.flowLayout = .init()
+            flowLayout.minimumInteritemSpacing = .interitemSpacing
             flowLayout.minimumLineSpacing = .rowLineSpacing
         }
     }
-}
-
-extension Input.Table.LayoutController {
-    
 }
 
 extension Input.Table.LayoutController: UICollectionViewDelegate {
@@ -47,21 +47,62 @@ extension Input.Table.LayoutController: UICollectionViewDelegateFlowLayout {
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: .sectionSpacing / 2, left: 0, bottom: .sectionSpacing / 2, right: 0)
+        return .init(top: .sectionSpacing / 2, left: collectionView.layoutMargins.left, bottom: .sectionSpacing / 2, right: collectionView.layoutMargins.right)
 
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let model = dataSource.model[indexPath.section][indexPath.row]
 
-        let availableWidth = collectionView.bounds.inset(by: collectionView.adjustedContentInset).width - collectionView.layoutMargins.left - collectionView.layoutMargins.right
+        let availableWidth = collectionView.bounds.inset(by: collectionView.adjustedContentInset).width
+        let cellWidth: CGFloat
 
-        let frame = CGRect(origin: .zero, size: CGSize(width: availableWidth, height: UIView.layoutFittingCompressedSize.height))
+        if isHalfWidthCell(at: indexPath, collectionView: collectionView) {
+            // -1 was added to compatability with previous iOS versions, summary value for 2 cells should be less than available width, not equal
+            cellWidth = (availableWidth - collectionView.layoutMargins.left - collectionView.layoutMargins.right - .interitemSpacing) / 2 - 1
+        } else {
+            // Add left and right spacing
+            cellWidth = availableWidth - collectionView.layoutMargins.left - collectionView.layoutMargins.right
+        }
+
+        let frame = CGRect(origin: .zero, size: CGSize(width: cellWidth, height: UIView.layoutFittingCompressedSize.height))
         let cell = model.cellType.init(frame: frame)
         try? model.configure(cell: cell)
 
         let autoLayoutSize = cell.systemLayoutSizeFitting(frame.size, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
 
         return autoLayoutSize
+    }
+
+    /// Half-width cells are expiry date and verification code cells following each other
+    private func isHalfWidthCell(at indexPath: IndexPath, collectionView: UICollectionView) -> Bool {
+        var isExpiryDatePresent = false
+        var isVerificationCodePresent = false
+        var halfWidthItemsPosition = [Int]()
+
+        for i in dataSource.model[indexPath.section].enumerated() {
+            if i.element is Input.Field.ExpiryDate {
+                isExpiryDatePresent = true
+                halfWidthItemsPosition.append(i.offset)
+            }
+            if i.element is Input.Field.VerificationCode {
+                isVerificationCodePresent = true
+                halfWidthItemsPosition.append(i.offset)
+            }
+        }
+
+        // If that item needs custom width
+        guard halfWidthItemsPosition.contains(indexPath.row) else { return false }
+
+        // If expiry and verification fields are present
+        guard isExpiryDatePresent && isVerificationCodePresent else { return false }
+
+        // Check that we have exactly 2 items, also out of bounds fatal error protection for the next `guard`
+        guard halfWidthItemsPosition.count == 2 else { return false }
+
+        // Check that items follow each other
+        guard (halfWidthItemsPosition[1] - halfWidthItemsPosition[0] == 1) else { return false }
+
+        return true
     }
 }
