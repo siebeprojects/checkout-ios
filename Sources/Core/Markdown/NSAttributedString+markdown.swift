6bccc185dfaded4c41ff550693dcf257cb8c850a
Vasily Ulianov <vasilyul@payoneer.com>
2021-09-02 19:45:29 +0300

    Revert "[PCX-1930] Add `contains` extension to NSAttributedString"
    This reverts commit 82852e3e8ea51e992450a6788678d874dfda8db1.
    
    # Conflicts:
    #	PayoneerCheckout.xcodeproj/project.pbxproj

--- Sources/Core/Markdown/NSAttributedString+markdown.swift
+++ Sources/Core/Markdown/NSAttributedString+markdown.swift
@@ -15,9 +15,3 @@ extension NSAttributedString {
         self.init(attributedString: attributedString)
     }
 }
-
-extension NSAttributedString {
-    func contains(attribute: NSAttributedString.Key) -> Bool {
-        return attributes(at: 0, effectiveRange: nil).contains { $0.key == attribute }
-    }
-}
