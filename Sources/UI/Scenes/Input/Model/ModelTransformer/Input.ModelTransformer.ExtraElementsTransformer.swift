89a322caf49be3e9244324ede79ab57d5207869f
Vasily Ulianov <vasilyul@payoneer.com>
2021-09-09 18:28:01 +0300

    [PCX-2073] Hide extra elements with checkbox
    + Move logic from extension to struct `ExtraElementsTransformer`

---
+++ Sources/UI/Scenes/Input/Model/ModelTransformer/Input.ModelTransformer.ExtraElementsTransformer.swift
@@ -0,0 +1,27 @@
+// Copyright (c) 2021 Payoneer Germany GmbH
+// https://www.payoneer.com
+//
+// This file is open source and available under the MIT license.
+// See the LICENSE file for more information.
+
+import Foundation
+
+extension Input.ModelTransformer {
+    struct ExtraElementsTransformer {
+        func createInputField(from extraElement: ExtraElement) -> InputField? {
+            guard extraElement.checkbox == nil else {
+                if #available(iOS 14.0, *) {
+                    logger.debug("Skipping extra element \(extraElement.name) because it has a checkbox")
+                }
+
+                return nil
+            }
+
+            let markdownParser = MarkdownParser()
+            let parsedLabel = markdownParser.parse(extraElement.label)
+            return Input.Field.Label(label: parsedLabel, name: extraElement.name, value: true.stringValue)
+        }
+    }
+}
+
+extension Input.ModelTransformer.ExtraElementsTransformer: Loggable {}
