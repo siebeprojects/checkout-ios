815ece88ab441982d0730b1bbfea3e921937b7ec
Caio Araujo <github@caiomello.com>
2021-09-10 16:15:51 +0200

    [PCX-2012] Extract constant

--- ExampleCheckout/UITests/DeleteButtonTests.swift
+++ ExampleCheckout/UITests/DeleteButtonTests.swift
@@ -6,82 +6,73 @@
 
 import XCTest
 
+/// Defined in https://optile.atlassian.net/browse/PCX-2012
 class DeleteButtonTests: NetworksTests {
+    let visa = Visa()
+
     func testDeleteButton_whenUpdateFlow_whenAllowDeleteIsTrue_shouldShow() throws {
-            let visa = Visa()
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .update)
+            transaction.allowDelete = true
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .update)
-                transaction.allowDelete = true
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 
-        func testDeleteButton_whenUpdateFlow_whenAllowDeleteIsFalse_shouldHide() throws {
-            let visa = Visa()
+    func testDeleteButton_whenUpdateFlow_whenAllowDeleteIsFalse_shouldHide() throws {
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .update)
+            transaction.allowDelete = false
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .update)
-                transaction.allowDelete = false
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 
-        func testDeleteButton_whenUpdateFlow_whenAllowDeleteIsNil_shouldShow() throws {
-            let visa = Visa()
+    func testDeleteButton_whenUpdateFlow_whenAllowDeleteIsNil_shouldShow() throws {
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .update)
+            transaction.allowDelete = nil
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .update)
-                transaction.allowDelete = nil
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 
-        func testDeleteButton_whenChargeFlow_whenAllowDeleteIsTrue_shouldShow() throws {
-            let visa = Visa()
+    func testDeleteButton_whenChargeFlow_whenAllowDeleteIsTrue_shouldShow() throws {
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .charge)
+            transaction.allowDelete = true
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .charge)
-                transaction.allowDelete = true
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertTrue(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 
-        func testDeleteButton_whenChargeFlow_whenAllowDeleteIsFalse_shouldHide() throws {
-            let visa = Visa()
+    func testDeleteButton_whenChargeFlow_whenAllowDeleteIsFalse_shouldHide() throws {
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .charge)
+            transaction.allowDelete = false
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .charge)
-                transaction.allowDelete = false
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 
-        func testDeleteButton_whenChargeFlow_whenAllowDeleteIsNil_shouldHide() throws {
-            let visa = Visa()
+    func testDeleteButton_whenChargeFlow_whenAllowDeleteIsNil_shouldHide() throws {
+        try XCTContext.runActivity(named: "Delete the payment method") { _ in
+            var transaction = try Transaction.loadFromTemplate(operationType: .charge)
+            transaction.allowDelete = nil
+            try setupWithPaymentSession(using: transaction)
 
-            try XCTContext.runActivity(named: "Delete the payment method") { _ in
-                var transaction = try Transaction.loadFromTemplate(operationType: .charge)
-                transaction.allowDelete = nil
-                try setupWithPaymentSession(using: transaction)
-
-                app.tables.staticTexts[visa.maskedLabel].tap()
-                XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
-            }
+            app.tables.staticTexts[visa.maskedLabel].tap()
+            XCTAssertFalse(app.navigationBars.buttons["Delete"].exists)
         }
+    }
 }
