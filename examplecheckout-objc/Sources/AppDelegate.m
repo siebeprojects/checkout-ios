4fc24561f396bfe45b8eb4c0a6a8b0be9da8ef97
Vasily Ulianov <vasilyul@payoneer.com>
2021-09-14 09:12:49 +0300

    [PCX-2102] Implement open url

--- examplecheckout-objc/Sources/AppDelegate.m
+++ examplecheckout-objc/Sources/AppDelegate.m
@@ -5,6 +5,7 @@
 // See the LICENSE file for more information.
 
 #import "AppDelegate.h"
+#import <PayoneerCheckout/PayoneerCheckout-Swift.h>
 
 @interface AppDelegate ()
 
@@ -28,12 +29,9 @@
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    [NSNotificationCenter.defaultCenter postNotificationName:NSNotification.didReceivePaymentResultURL object:url];
+    return YES;
 }
-
 
 @end
