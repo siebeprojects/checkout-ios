596d5e3e9c3bba864db2390edda0b2496df140ce
Vasily Ulianov <vasilyul@payoneer.com>
2021-09-28 19:09:46 +0300

    [PCX-2021] Change comments

--- Sources/UI/Scenes/Input/Model/Field/Input.Field.Identifier.swift
+++ Sources/UI/Scenes/Input/Model/Field/Input.Field.Identifier.swift
@@ -7,24 +7,24 @@
 import Foundation
 
 extension Input.Field {
-    /// Identifier for input fields.
+    /// An identifier for input fields.
     ///
     /// It is used to have a clear identification of input field and to separate server models, ui models and payment request models that may seem that they used for the same entity but they're not.
-    /// - Note: identifier doesn't always equal server's model name (for the exception of `inputElementName` case).
+    /// - Note: an identifier doesn't always equal server's model name (for the exception of `inputElementName` case).
     enum Identifier {
-        /// Identifier for expiry date combined field's model (*mm / yy*)
+        /// The identifier for expiry date combined field's model (*mm / yy*)
         case expiryDate
 
-        /// Identifier for allow / auto registration checkbox or label
+        /// The identifier for registration switch or label
         case registration
 
-        /// Identifier for allow recurrence checkbox or label
+        /// The identifier for recurrence switch or label
         case recurrence
 
-        /// Identifier for combined registration and recurrence registration switch
+        /// The identifier for combined registration and recurrence registration switch
         case combinedRegistration
 
-        /// Identifier for field that uses server's `name` from input element
+        /// The identifier for field that uses server's `name` from input element
         case inputElementName(String)
     }
 }
