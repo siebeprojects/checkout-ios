e29e328c99c54fa0c3839a0ec7882d857a39f6ef
Caio Araujo <github@caiomello.com>
2021-11-19 11:09:17 +0100

    [PCX-2119] Disable animations in TextInputView

--- Sources/UI/Components/TextInputView/TextInputView.swift
+++ Sources/UI/Components/TextInputView/TextInputView.swift
@@ -101,6 +101,9 @@ extension TextInputView {
     }
 
     private func updateAppearance(animated: Bool) {
+        // Animations are turned off for now so they don't conflict with UICollectionView reload animations.
+        let animated = false
+
         // If a label is in a UIStackView and its text is set to nil, the label is automatically hidden by the UIStackView.
         // This mechanic breaks the show/hide animations being done here, therefore errorMessage can't be optional and an empty string is used instead.
         let configuration: (borderColor: UIColor, borderWidth: CGFloat, errorMessage: String) = {
@@ -139,11 +142,12 @@ extension TextInputView {
             self?.errorLabel.superview?.layoutIfNeeded()
         }
 
-        if animated {
-            animate(changes)
-        } else {
+        // Animations are turned off for now so they don't conflict with UICollectionView reload animations.
+//        if animated {
+//            animate(changes)
+//        } else {
             changes()
-        }
+//        }
     }
 }
 
