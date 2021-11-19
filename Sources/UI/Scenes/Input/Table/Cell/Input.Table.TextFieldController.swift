6cd77474445c547b391eee9596ddcf8f2f7dec77
Caio Araujo <github@caiomello.com>
2021-11-19 14:20:32 +0100

    [PCX-2119] Fix duplicated extension method

--- Sources/UI/Scenes/Input/Table/Cell/Input.Table.TextFieldController.swift
+++ Sources/UI/Scenes/Input/Table/Cell/Input.Table.TextFieldController.swift
@@ -231,12 +231,3 @@ private extension UITextField {
         }
     }
 }
-
-extension UIFont {
-    func withWeight(_ weight: UIFont.Weight) -> UIFont {
-        let newDescriptor = fontDescriptor.addingAttributes([
-            .traits: [UIFontDescriptor.TraitKey.weight: weight]
-        ])
-        return UIFont(descriptor: newDescriptor, size: pointSize)
-    }
-}
