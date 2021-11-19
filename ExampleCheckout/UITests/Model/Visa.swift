eaaf43153c2ecb4a64da2029c06debc2440abf55
Caio Araujo <github@caiomello.com>
2021-11-19 14:49:03 +0100

    [PCX-2119] Fix card UI tests

--- ExampleCheckout/UITests/Model/Visa.swift
+++ ExampleCheckout/UITests/Model/Visa.swift
@@ -22,7 +22,7 @@ struct Visa: PaymentNetwork {
     func fill(in collectionView: XCUIElementQuery) {
         XCTContext.runActivity(named: "Fill VISA card's data") { _ in
             tapAndType(text: number, in: collectionView.textFields["13 to 19 digits"])
-            tapAndType(text: expiryDate, in: collectionView.textFields["Valid thru"])
+            tapAndType(text: expiryDate, in: collectionView.textFields["MM / YY"])
             tapAndType(text: verificationCode, in: collectionView.textFields["3 digits"])
             tapAndType(text: holderName, in: collectionView.textFields["e.g. John Doe"])
         }
