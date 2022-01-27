63e5b008f3dbf2cc8c5c71b1c3a05f1b633fbbbe
Caio Araujo <github@caiomello.com>
2022-01-27 10:05:52 +0100

    [PCX-2402] Unify UI tests

--- ExampleCheckout/UITests/ExpiredCardTests.swift
+++ ExampleCheckout/UITests/ExpiredCardTests.swift
@@ -34,19 +34,27 @@ final class ExpiredCardTests: NetworksTests {
             try setupWithPaymentSession(transaction: transaction)
 
             XCTAssertTrue(app.cells.element(boundBy: 0).staticTexts["10 / \(ExpiredCardTests.twoDigitNextYear)"].exists)
-        }
-    }
-
-    func testPaymentList_whenNotCard_shouldNotShowExpirationDate() throws {
-        try XCTContext.runActivity(named: "Show card expiration date") { _ in
-            let transaction = try Transaction.create(withSettings: TransactionSettings(operationType: .update, customerId: Self.validCardCustomerID))
-            try setupWithPaymentSession(transaction: transaction)
 
             for index in 1...3 {
                 let cell = app.cells.element(boundBy: index)
                 XCTAssertTrue(cell.exists, "Cell with a network doesn't exist, couldn't check absense of expiration date")
                 XCTAssertFalse(cell.staticTexts["10 / \(ExpiredCardTests.twoDigitNextYear)"].exists)
             }
+        }
+    }
+
+    func testPaymentList_whenExpiredCard_shouldShowExpirationDate_shouldShowExpirationInfoAlert() throws {
+        try XCTContext.runActivity(named: "Show card expiration date") { _ in
+            let transaction = try Transaction.create(withSettings: TransactionSettings(operationType: .update, customerId: Self.expiredCardCustomerID))
+            try setupWithPaymentSession(transaction: transaction)
+
+            let cell = app.cells.element(boundBy: 0)
+
+            XCTAssertTrue(cell.staticTexts["10 / 20"].exists)
+
+            cell.buttons["expirationInfo"].tap()
+
+            XCTAssertTrue(app.alerts.firstMatch.exists)
         }
     }
 
@@ -58,26 +66,6 @@ final class ExpiredCardTests: NetworksTests {
             app.cells.element(boundBy: 0).tap()
 
             XCTAssertTrue(app.collectionViews.staticTexts["10 / \(ExpiredCardTests.twoDigitNextYear)"].exists)
-        }
-    }
-
-    func testPaymentList_whenExpiredCard_shouldShowExpirationDate() throws {
-        try XCTContext.runActivity(named: "Show card expiration date") { _ in
-            let transaction = try Transaction.create(withSettings: TransactionSettings(operationType: .update, customerId: Self.expiredCardCustomerID))
-            try setupWithPaymentSession(transaction: transaction)
-
-            XCTAssertTrue(app.cells.element(boundBy: 0).staticTexts["10 / 20"].exists)
-        }
-    }
-
-    func testPaymentList_whenExpiredCard_shouldShowExpirationInfoAlert() throws {
-        try XCTContext.runActivity(named: "Show card expiration date") { _ in
-            let transaction = try Transaction.create(withSettings: TransactionSettings(operationType: .update, customerId: Self.expiredCardCustomerID))
-            try setupWithPaymentSession(transaction: transaction)
-
-            app.cells.element(boundBy: 0).buttons["expirationInfo"].tap()
-
-            XCTAssertTrue(app.alerts.firstMatch.exists)
         }
     }
 
