30bed95a25b75ba2e87bf787e9a2676f836a45a5
Caio Araujo <github@caiomello.com>
2022-02-28 10:24:47 +0100

    [PCX-2795] Add navigation tests

---
+++ ExampleCheckout/UITests/NavigationTests.swift
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Payoneer Germany GmbH
+// https://www.payoneer.com
+//
+// This file is open source and available under the MIT license.
+// See the LICENSE file for more information.
+
+import XCTest
+
+final class NavigationTests: NetworksTests {
+    func testClose() throws {
+        let transaction = try Transaction.create(withSettings: TransactionSettings())
+        try setupWithPaymentSession(transaction: transaction)
+
+        let closeButton = app.buttons["Close"]
+
+        XCTAssertTrue(closeButton.exists)
+
+        closeButton.tap()
+
+        let expectation = expectation(for: NSPredicate(format: "exists == NO"), evaluatedWith: closeButton)
+        wait(for: [expectation], timeout: .uiTimeout)
+
+        XCTAssertFalse(closeButton.exists)
+    }
+}
