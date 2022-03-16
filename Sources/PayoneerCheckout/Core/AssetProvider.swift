d7186bc0e3840ad915a911ae9c28cfd0454c7d55
Caio Araujo <github@caiomello.com>
2022-03-16 17:10:41 +0100

    [PCX-2554] Implement appearance customization (WIP)

--- Sources/PayoneerCheckout/Core/AssetProvider.swift
+++ Sources/PayoneerCheckout/Core/AssetProvider.swift
@@ -6,31 +6,23 @@
 
 import UIKit
 
-final class AssetProvider {
+enum Colors {
+    static let primaryText = UIColor(named: "PrimeryText", in: .current, compatibleWith: nil)!
+    static let secondaryText = UIColor(named: "SecondaryText", in: .current, compatibleWith: nil)!
+    static let background = UIColor(named: "Background", in: .current, compatibleWith: nil)!
+    static let accent = UIColor(named: "Accent", in: .current, compatibleWith: nil)!
+    static let error = UIColor(named: "Error", in: .current, compatibleWith: nil)!
+    static let border = UIColor(named: "Border", in: .current, compatibleWith: nil)!
+}
+
+enum AssetProvider {
     #if canImport(UIKit)
-    static var iconCard: UIImage? {
-        return UIImage(named: "iconCard", in: .current, compatibleWith: nil)
-    }
-
-    static var iconCVVQuestionMark: UIImage? {
-        return UIImage(named: "iconCVVQuestionMark", in: .current, compatibleWith: nil)
-    }
-
-    static var cvvCard: UIImage? {
-        return UIImage(named: "cvvCard", in: .current, compatibleWith: nil)
-    }
-
-    static var cvvAMEX: UIImage? {
-        return UIImage(named: "cvvAMEX", in: .current, compatibleWith: nil)
-    }
-
-    static var iconClear: UIImage? {
-        return UIImage(named: "iconClear", in: .current, compatibleWith: nil)
-    }
-
-    static var expirationInfo: UIImage? {
-        return UIImage(named: "expirationInfo", in: .current, compatibleWith: nil)
-    }
+    static let iconCard = UIImage(named: "iconCard", in: .current, compatibleWith: nil)
+    static let iconCVVQuestionMark = UIImage(named: "iconCVVQuestionMark", in: .current, compatibleWith: nil)
+    static let cvvCard = UIImage(named: "cvvCard", in: .current, compatibleWith: nil)
+    static let cvvAMEX = UIImage(named: "cvvAMEX", in: .current, compatibleWith: nil)
+    static let iconClear = UIImage(named: "iconClear", in: .current, compatibleWith: nil)
+    static let expirationInfo = UIImage(named: "expirationInfo", in: .current, compatibleWith: nil)
 
     static var disclosureIndicator: UIImage? {
         if #available(iOS 13.0, *) {
--- Sources/PayoneerCheckout/Interface/CheckoutAppearance.swift
+++ Sources/PayoneerCheckout/Interface/CheckoutAppearance.swift
@@ -8,20 +8,33 @@ import UIKit
 
 /// An object containing appearance-related settings for the checkout UI.
 @objc public class CheckoutAppearance: NSObject {
+    public let font: UIFont
     public let primaryTextColor: UIColor
     public let secondaryTextColor: UIColor
+    public let backgroundColor: UIColor
+    public let accentColor: UIColor
     public let errorColor: UIColor
-
-    // TODO: Use correct colors
+    public let borderColor: UIColor
+    public let buttonTitleColor: UIColor
 
     /// Initializes a `CheckoutAppearance` with the given parameters.
     @objc public init(
-        primaryTextColor: UIColor = .black,
-        secondaryTextColor: UIColor = .gray,
-        errorColor: UIColor = .red
+        font: UIFont? = nil,
+        primaryTextColor: UIColor? = nil,
+        secondaryTextColor: UIColor? = nil,
+        backgroundColor: UIColor? = nil,
+        accentColor: UIColor? = nil,
+        errorColor: UIColor? = nil,
+        borderColor: UIColor? = nil,
+        buttonTitleColor: UIColor? = nil
     ) {
-        self.primaryTextColor = primaryTextColor
-        self.secondaryTextColor = secondaryTextColor
-        self.errorColor = errorColor
+        self.font = font ?? .preferredFont(forTextStyle: .body)
+        self.primaryTextColor = primaryTextColor ?? Colors.primaryText
+        self.secondaryTextColor = secondaryTextColor ?? Colors.secondaryText
+        self.backgroundColor = backgroundColor ?? Colors.background
+        self.accentColor = accentColor ?? Colors.accent
+        self.errorColor = errorColor ?? Colors.error
+        self.borderColor = borderColor ?? Colors.border
+        self.buttonTitleColor = buttonTitleColor ?? .white
     }
 }
