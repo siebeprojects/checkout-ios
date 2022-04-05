233d83a746af48de5536c8bdf3bada56e19e4b5b
Caio Araujo <github@caiomello.com>
2022-04-05 16:54:05 +0200

    [PCX-2554] Add documentation to appearance class and font protocol

--- Sources/PayoneerCheckout/Interface/CheckoutAppearance.swift
+++ Sources/PayoneerCheckout/Interface/CheckoutAppearance.swift
@@ -8,18 +8,34 @@ import UIKit
 
 /// An object containing appearance-related settings for the checkout UI.
 @objc public class CheckoutAppearance: NSObject {
+    /// The color of primary text, like titles and body.
     public let primaryTextColor: UIColor
+
+    /// The color of secondary text, like subtitles and captions.
     public let secondaryTextColor: UIColor
+
+    /// The background color of screens and views.
     public let backgroundColor: UIColor
+
+    /// The accent color. On iOS this is the tint color. If `nil`, the host app's tint color is used.
     public let accentColor: UIColor?
+
+    /// The color used to indicate errors. It's used in text fields.
     public let errorColor: UIColor
+
+    /// The color of the borders and row separators on the payment list screen.
     public let borderColor: UIColor
+
+    /// The color of the primary button's title.
     public let buttonTitleColor: UIColor
+
+    /// An object responsible for providing the different variations of a custom font. If `nil`, the default system font will be used.
     public let fontProvider: CheckoutFontProviderProtocol
 
     /// The shared singleton appearance object. Initialized by `Checkout`.
     static var shared: CheckoutAppearance = .default
 
+    /// The default appearance used if no custom appearance is set.
     @objc public static var `default`: CheckoutAppearance {
         CheckoutAppearance(
             primaryTextColor: Colors.primaryText,
@@ -33,6 +49,15 @@ import UIKit
     }
 
     /// Initializes a `CheckoutAppearance` with the given parameters.
+    /// - Parameters:
+    ///   - primaryTextColor: The color of primary text, like titles and body.
+    ///   - secondaryTextColor: The color of secondary text, like subtitles and captions.
+    ///   - backgroundColor: The background color of screens and views.
+    ///   - accentColor: The accent color. On iOS this is the tint color. If `nil`, the host app's tint color is used.
+    ///   - errorColor: The color used to indicate errors. It's used in text fields.
+    ///   - borderColor: The color of the borders and row separators on the payment list screen.
+    ///   - buttonTitleColor: The color of the primary button's title.
+    ///   - fontProvider: An object responsible for providing the different variations of a custom font. If `nil`, the default system font will be used.
     @objc public init(
         primaryTextColor: UIColor = CheckoutAppearance.default.primaryTextColor,
         secondaryTextColor: UIColor = CheckoutAppearance.default.secondaryTextColor,
--- Sources/PayoneerCheckout/Interface/CheckoutFontProviderProtocol.swift
+++ Sources/PayoneerCheckout/Interface/CheckoutFontProviderProtocol.swift
@@ -6,7 +6,13 @@
 
 import UIKit
 
+/// The protocol used to create a custom font provider.
 @objc public protocol CheckoutFontProviderProtocol {
+    /// Returns an instance of the custom font for the specified text style with scaling for the user's selected content size category.
+    /// - Returns: The custom font associated with the specified text style.
     func font(forTextStyle textStyle: UIFont.TextStyle) -> UIFont
+
+    /// Returns the custom font object for standard interface items in the specified size and weight.
+    /// - Returns: A font object of the specified size and weight.
     func font(ofSize fontSize: CGFloat, weight: UIFont.Weight) -> UIFont
 }
