siebeprojects/checkout-ios

commit d46680adc15a3048a0068111650fc82c664f2acf

[PCX-3143] Use less code
Co-authored-by: Caio Mello <github@caiomello.com>

Sources/PaymentServices/BasicPaymentService/BasicPaymentService.swift
@@ -12,27 +12,13 @@ import UIKit
 @objc public class BasicPaymentService: NSObject, PaymentService {
     // MARK: - Static methods
     public static func isSupported(networkCode: String, paymentMethod: String?) -> Bool {
-        if let paymentMethod = paymentMethod {
-            if isSupported(method: paymentMethod) { return true }
+        if let methodString = paymentMethod, let method = PaymentMethod(rawValue: methodString) {
+            let supportedMethods: [PaymentMethod] = [.DEBIT_CARD, .CREDIT_CARD]
+            return supportedMethods.contains(method)
+        } else {
+            let supportedCodes = ["SEPADD", "PAYPAL", "WECHATPC-R"]
+            return supportedCodes.contains(networkCode)
         }
-
-        if isSupported(code: networkCode) { return true }
-
-        return false
-    }
-
-    private static func isSupported(method: String) -> Bool {
-        let supportedMethods: [PaymentMethod] = [.DEBIT_CARD, .CREDIT_CARD]
-        guard let paymentMethod = PaymentMethod(rawValue: method) else {
-            return false
-        }
-
-        return supportedMethods.contains(paymentMethod)
-    }
-
-    private static func isSupported(code: String) -> Bool {
-        let supportedCodes = ["SEPADD", "PAYPAL", "WECHATPC-R"]
-        return supportedCodes.contains(code)
     }
 
     // MARK: -

commit 2aa3fe84accdf3d180a95bd38b66adb26065fe37

[PCX-3100] Fix a bug introduced in d46680adc15a3048a0068111650fc82c664f2acf

Sources/PaymentServices/BasicPaymentService/BasicPaymentService.swift
@@ -19,11 +19,11 @@ import Redirection
     public static func isSupported(networkCode: String, paymentMethod: String?) -> Bool {
         if let methodString = paymentMethod, let method = PaymentMethod(rawValue: methodString) {
             let supportedMethods: [PaymentMethod] = [.DEBIT_CARD, .CREDIT_CARD]
-            return supportedMethods.contains(method)
-        } else {
-            let supportedCodes = ["SEPADD", "PAYPAL", "WECHATPC-R"]
-            return supportedCodes.contains(networkCode)
+            if supportedMethods.contains(method) { return true }
         }
+
+        let supportedCodes = ["SEPADD", "PAYPAL", "WECHATPC-R"]
+        return supportedCodes.contains(networkCode)
     }
 
     // MARK: - Initializer