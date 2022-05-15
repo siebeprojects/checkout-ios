0f9586ba5644b80a9db94f3bb749c3ab0ff36516
Vasily Ulianov <vasilyul@payoneer.com>
2022-05-15 22:59:25 +0300

    [PCX-2894] Add a script to delete app from Browserstack

---
+++ .github/scripts/browserstack_delete.py
@@ -0,0 +1,54 @@
+import requests
+import urllib.parse
+import sys
+import os
+
+# Browserstack API: https://www.browserstack.com/app-live/rest-api
+
+session = requests.Session()
+
+
+def main():
+    try:
+        session.auth = (os.environ['BROWSERSTACK_USER'],
+                        os.environ['BROWSERSTACK_KEY'])
+
+        # First argument from command line is custom_id for Browserstack
+        custom_id = sys.argv[1]
+        recent_uploads = get_recent_uploads(custom_id)
+        for app in recent_uploads:
+            delete(app)
+
+    except Exception as error:
+        sys.exit(f'::error::{error}')
+
+
+def get_recent_uploads(custom_id):
+    """Get recent uploads from Browserstack"""
+
+    recent_apps_url = urllib.parse.urljoin(
+        'https://api-cloud.browserstack.com/app-live/recent_apps/', custom_id)
+    recent_uploads = session.get(recent_apps_url)
+    recent_uploads.raise_for_status()
+    return recent_uploads.json()
+
+
+def delete(app):
+    """Delete an application from Browserstack live
+
+        Parameters:
+        app: application json object from Browserstack recent_apps call
+    """
+
+    app_id = app['app_id']
+    app_name = app['app_name']
+    delete_url = urllib.parse.urljoin(
+        'https://api-cloud.browserstack.com/app-live/app/delet/', app_id)
+    print(f'* Deleting {app_name}…')
+
+    deletion_result = session.delete(delete_url)
+    deletion_result.raise_for_status()
+
+
+if __name__ == "__main__":
+    main()
