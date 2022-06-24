b32206d01c22982a58e8d061bffa855e6e05e98b
Caio Araujo <github@caiomello.com>
2022-06-24 17:14:28 +0200

    [PCX-3455] Add tests for GetRequest

---
+++ Tests/NetworkingTests/GetRequestTests.swift
@@ -0,0 +1,51 @@
+// Copyright (c) 2022 Payoneer Germany GmbH
+// https://www.payoneer.com
+//
+// This file is open source and available under the MIT license.
+// See the LICENSE file for more information.
+
+import XCTest
+@testable import Networking
+@testable import Logging
+
+private final class MockRequest: GetRequest, Loggable {
+    typealias Response = String
+
+    var httpMethod: HTTPMethod { .GET }
+    let queryItems: [URLQueryItem]
+    let url: URL
+
+    init(queryItems: [URLQueryItem], url: URL) {
+        self.queryItems = queryItems
+        self.url = url
+    }
+
+    func logRequest() {}
+    func logResponse(_ response: String) {}
+}
+
+final class GetRequestTests: XCTestCase {
+    func testHTTPMethod_shouldReturnGet() {
+        let getRequest = MockRequest(queryItems: [], url: URL(string: "/")!)
+        XCTAssertEqual(getRequest.httpMethod, .GET)
+    }
+
+    func testBuild_shouldSetURLRequestHTTPMethod() throws {
+        let getRequest = MockRequest(queryItems: [], url: URL(string: "/")!)
+        let urlRequest = try getRequest.build()
+        XCTAssertEqual(urlRequest.httpMethod, getRequest.httpMethod.rawValue)
+    }
+
+    func testBuild_shouldSetURLRequestURL() throws {
+        let getRequest = MockRequest(queryItems: [], url: URL(string: "/")!)
+        let urlRequest = try getRequest.build()
+        XCTAssertEqual(urlRequest.url, getRequest.url)
+    }
+
+    func testBuild_whenQueryItems_shouldSetURLRequestQueryItems() throws {
+        let getRequest = MockRequest(queryItems: [URLQueryItem(name: "a", value: "a")], url: URL(string: "/")!)
+        let urlRequest = try getRequest.build()
+
+        XCTAssertEqual(urlRequest.url?.absoluteString, "\(getRequest.url.absoluteString)?a=a")
+    }
+}
