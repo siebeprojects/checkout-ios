de200b8bdf7dde0d7a88a045a4c4adcc1565012f
Vasilii Ulianov <vasilyul@payoneer.com>
2022-07-25 13:22:47 +0300

    [PCX-3383] Change year in copyright

--- Sources/PayoneerCheckout/UI/Scenes/Input/Model/ModelTransformer/Input.ModelTransformer.ExtraElementsTransformer.swift
+++ Sources/PayoneerCheckout/UI/Scenes/Input/Model/ModelTransformer/Input.ModelTransformer.ExtraElementsTransformer.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2021–2022 Payoneer Germany GmbH
+// Copyright (c) 2022 Payoneer Germany GmbH
 // https://www.payoneer.com
 //
 // This file is open source and available under the MIT license.
